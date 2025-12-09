7188120c8c195669fbe37bf0a361d484e3603ccb
josuesoares64 <josuesoares318@gmail.com>
2025-12-09 19:23:30 -0300

    Teste

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const routes = require('./routes');
+const sequelize = require('./config/database'); // <-- IMPORTANTE
 
 const app = express();
+
+app.use(express.json()); // caso não tenha
+
+// Sincroniza todas as tabelas do Sequelize
+sequelize.sync()
+  .then(() => console.log("Banco sincronizado!"))
+  .catch((err) => console.error("Erro ao sincronizar o banco:", err));
 
 routes(app);
 
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 3000");
 });
-
 
 module.exports = app;
